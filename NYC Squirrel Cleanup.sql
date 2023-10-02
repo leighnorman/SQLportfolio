b0c41732ba565f33ea6dd3587ef60854de4073df
l_norman_port <89787410+leighnorman@users.noreply.github.com>
2023-10-02 18:18:03 -0400

    Update NYC Squirrel Cleanup.sql
    Updated wording and added new default constraint line.

--- NYC Squirrel Cleanup.sql
+++ NYC Squirrel Cleanup.sql
@@ -32,8 +32,14 @@ HAVING COUNT(*) > 1;
 --To save myself a headache (and shaking down Google for answers), I'll add a primary key constraint so the same data doesn't get added more than once. 
 ALTER TABLE [2018 Central Park Squirrels]
 ADD CONSTRAINT PK_Unique_Squirrel_ID PRIMARY KEY (unique_squirrel_id);
+--This will be especially helpful for future collections. Now they can't make this unique squirrel ID error!
 
---This will be especially helpful to volunteers in the future. Now they can't make this unique squirrel ID error!
+--I can also add a default value for the subjective fields. I'll put an example below, since there are too many subjective fields to do all these defaults for right now.
+-- I'll fix "Other Interactions" since that one was almost always blank. 
+ALTER TABLE [2018 Central Park Squirrels]
+ADD CONSTRAINT Other_Interactions
+DEFAULT 'None listed';
+--A default constraint makes more sense here. Now, volunteers won't receive an error for fields they weren't required to fill out, nor will the dataset have so many NULLs.
 
 --Now then, let's move on to the full dataset. 
 SELECT *
@@ -41,7 +47,7 @@ FROM [2018 Central Park Squirrels];
 
 --I am seeing many NULLs. This is expected, since some of this data is subjective, optional text fields. 
 --For Color Notes, the primer says "Sighters occasionally added commentary on the squirrel fur conditions."
---Because of this, NULLs here doesn't suggest missing data. 
+--Because of this, NULLs here doesn't suggest missing data, just optional fields. 
 --Other Activities, Other Interactions, and Specific Location are all optional, descriptive fields
 --I'm going to replace the NULLs with 'none noted'. This term fits how the data was collected: it's not that the squirrel didn't have any highlights *at all*, just that the volunteer didn't make a note of it. 
 
@@ -61,10 +67,6 @@ SET Specific_Location = 'None noted'
 WHERE Specific_Location IS NULL;
 
 UPDATE dbo.[2018 Central Park Squirrels]
-SET Other_Interactions = 'None noted'
-WHERE Other_Interactions IS NULL;
-
-UPDATE dbo.[2018 Central Park Squirrels]
 SET Highlight_Fur_Color = 'None noted'
 WHERE Highlight_Fur_Color IS NULL;
 
@@ -72,7 +74,7 @@ UPDATE dbo.[2018 Central Park Squirrels]
 SET Primary_Fur_Color = 'None noted'
 WHERE Primary_Fur_Color IS NULL;
 
---That's the colors and interactions done. "Above Ground Sighter Measurement" also has NULLs. But "none noted" doesn't make sense for a numeric description (even though it's store as text)
+--That's the colors and location done. "Above Ground Sighter Measurement" also has NULLs. But "none noted" doesn't make sense for a numeric description (even though it's store as text)
 --This column is odd because it's not one datatype - I made it "nvar" because it can be FALSE (squirrel wasn't above ground/was on ground) or a number (height from the ground).
 --How many NULLs are there?
 
@@ -80,8 +82,8 @@ SELECT COUNT(*)
 FROM dbo.[2018 Central Park Squirrels]
 WHERE Above_Ground_Sighter_Measurement IS NULL;
 
---114 NULLs - that's significant enough to update them. 
---Saying '0' doesn't make sense, since the dataset already established 'FALSE' as its '0'. I could change the FALSEs to 0, though. The Eastern Grey Squirrel doesn't burrow, so a squirrel's measurement wouldn't be less than 0.
+--114 NULLs - that's significant enough to update them. The problem now is what are those NULLs telling me?  
+--Saying '0' doesn't make sense, since the dataset already established 'FALSE' as its '0'. I could change the FALSEs to 0, though. Eastern Grey Squirrel don't burrow (i.e. having an above ground measure of less than 1), so a squirrel's measurement wouldn't be less than 0.
 --That still doesn't help with the NULL issue. I'm going to put 'N/A' for now. If I change my mind, I can UPDATE later.
 
 UPDATE dbo.[2018 Central Park Squirrels]
